49c87d07c431c457a10c3018c26318b038f2bb12
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-11-23 05:27:24 -0700

    Release version 2.0

--- src/version.h
+++ src/version.h
@@ -12,6 +12,6 @@
 
 #pragma once
 
-#define PLUGIN_VERSION_STR	    "1.9.0"
-#define RESOURCE_VERSION         1,9,0,0
-#define RESOURCE_VERSION_STR    "1.9.0.0"
+#define PLUGIN_VERSION_STR	    "2.0.0"
+#define RESOURCE_VERSION         2,0,0,0
+#define RESOURCE_VERSION_STR    "2.0.0.0"
