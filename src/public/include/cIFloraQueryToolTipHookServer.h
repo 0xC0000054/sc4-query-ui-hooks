4e96fceaf48281581816d23307229688903a187c
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-02-04 05:04:15 -0700

    Bump copyright year

--- src/public/include/cIFloraQueryToolTipHookServer.h
+++ src/public/include/cIFloraQueryToolTipHookServer.h
@@ -3,7 +3,7 @@
 // This file is part of sc4-query-ui-hooks, a DLL Plugin for SimCity 4 that
 // extends the query UI.
 //
-// Copyright (c) 2024 Nicholas Hayes
+// Copyright (c) 2024, 2025 Nicholas Hayes
 //
 // This file is licensed under terms of the MIT License.
 // See LICENSE.txt for more information.
